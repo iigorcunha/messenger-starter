bccb113a991e0800b3170eea525eec169b1ad2d4
Igor Cunha <igorcesarcunha@gmail.com>
2021-06-21 16:29:35 -0400

    remove all references to localStorage

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -8,12 +8,6 @@ import {
 } from "../conversations";
 import { gotUser, setFetchingStatus } from "../user";
 
-axios.interceptors.request.use(async function (config) {
-  const token = await localStorage.getItem("messenger-token");
-  config.headers["x-access-token"] = token;
-
-  return config;
-});
 
 // USER THUNK CREATORS
 
@@ -35,11 +29,12 @@ export const fetchUser = () => async (dispatch) => {
 export const register = (credentials) => async (dispatch) => {
   try {
     const { data } = await axios.post("/auth/register", credentials);
-    await localStorage.setItem("messenger-token", data.token);
+    
+    
     dispatch(gotUser(data));
     socket.emit("go-online", data.id);
   } catch (error) {
-    console.error(error);
+    console.error(error.response.data);
     dispatch(gotUser({ error: error.response.data.error || "Server Error" }));
   }
 };
@@ -47,7 +42,7 @@ export const register = (credentials) => async (dispatch) => {
 export const login = (credentials) => async (dispatch) => {
   try {
     const { data } = await axios.post("/auth/login", credentials);
-    await localStorage.setItem("messenger-token", data.token);
+    console.log(data);
     dispatch(gotUser(data));
     socket.emit("go-online", data.id);
   } catch (error) {
@@ -59,7 +54,6 @@ export const login = (credentials) => async (dispatch) => {
 export const logout = (id) => async (dispatch) => {
   try {
     await axios.delete("/auth/logout");
-    await localStorage.removeItem("messenger-token");
     dispatch(gotUser({}));
     socket.emit("logout", id);
   } catch (error) {
