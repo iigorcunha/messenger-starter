eff7ca2a4e86e3655b8c0156c7c209c0649e620c
Igor Cunha <igorcesarcunha@gmail.com>
2021-06-23 20:18:31 -0400

    add dotenv.config to read properly

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const Sequelize = require("sequelize");
+
 
 const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
   logging: false
