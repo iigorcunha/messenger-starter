fb9da45ea819fc61dd285f3afed1191b298cb1f2
Igor Cunha <igorcesarcunha@gmail.com>
2021-06-25 17:28:04 -0400

    add a memo to pick last read message

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
@@ -6,15 +6,34 @@ import moment from "moment";
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
+  const lastReadMessage = useMemo(() => {
+    const listOfReadMessages = messages.filter(message => message.senderId === userId)
+    .filter(filteredMessage => filteredMessage.recipientRead === true)
+    const lastReadMessage = listOfReadMessages[listOfReadMessages.length - 1]
+
+    return lastReadMessage;
+  } , [messages, userId])
+
   return (
     <Box>
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <SenderBubble 
+            key={message.id} 
+            text={message.text}
+            time={time}
+            otherUser={otherUser}  
+            lastReadMessage={lastReadMessage && lastReadMessage.id === message.id} />
+            
         ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+          <OtherUserBubble 
+            key={message.id} 
+            text={message.text} 
+            time={time}
+            otherUser={otherUser} 
+            />
         );
       })}
     </Box>
