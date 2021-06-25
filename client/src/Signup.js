5bd9983e62207dc8fa4223f6fe8946e1a763f521
Igor Cunha <igorcesarcunha@gmail.com>
2021-06-25 19:19:00 -0400

    Create signUp Page

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -2,20 +2,115 @@ import React, { useState } from "react";
 import { Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import {
-  Grid,
   Box,
   Typography,
   Button,
   FormControl,
   TextField,
-  FormHelperText,
 } from "@material-ui/core";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { makeStyles } from "@material-ui/core/styles";
 import { register } from "./store/utils/thunkCreators";
 
-const Login = (props) => {
+import BgImage from "./assets/images/bg-img.png";
+import BubbleSvg from "./assets/images/bubble.svg";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    height: "100vh"
+  },
+  leftSide: {
+    backgroundImage: `url(${BgImage})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: "cover",
+    width: "35%",
+  },
+  cover: {
+    background: "linear-gradient(0deg, rgba(134,185,255,1) 0%, rgba(58,141,255,1) 100%)",
+    height: "100%",
+    opacity: 0.85,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+
+  image: {
+    height: "96px",
+    marginBottom: "2rem",
+  },
+
+  title: {
+    fontFamily: "Open Sans",
+    fontSize: 36,
+    color: "#fff",
+
+  },
+
+  rightSide: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    position: "relative",
+  },
+  inputContainer: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    marginBottom: "4rem"
+  },
+  input: {
+    paddingTop: "1rem",
+  },
+  header: {
+    width: "100%",
+    display: "flex",
+    margin: "4rem",
+    marginRight: "12rem",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    color: "#c7c7c7"
+  },
+  registerButton: {
+    marginLeft: "4rem",
+    width: "12rem",
+    height: "3rem",
+    boxShadow: "0 2px 5px rgba(80,80,80,0.1)",
+    color: "#3A8DFF",
+  },
+  form: {
+    width: "40%",
+    flex: 1,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  titleFormContainer: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "flex-start",
+    marginBottom: "4rem",
+  },
+  titleForm: {
+    fontSize: "3rem",
+  },
+  buttonSubmit: {
+    height: "4.5rem",
+    width: "14rem",
+  }
+}));
+
+const SignUp = (props) => {
   const history = useHistory();
   const { user, register } = props;
-  const [formErrorMessage, setFormErrorMessage] = useState({});
+  const [setFormErrorMessage] = useState({});
+  const matches = useMediaQuery('(min-width:720px)');
+
+  const classes = useStyles(props);
 
   const handleRegister = async (event) => {
     event.preventDefault();
@@ -37,73 +132,59 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to log in?</Typography>
-          <Button onClick={() => history.push("/login")}>Login</Button>
-        </Grid>
-        <form onSubmit={handleRegister}>
-          <Grid>
-            <Grid>
-              <FormControl>
+    <Box container justify="center" className={classes.root}>
+    {matches && (<Box component="div" className={classes.leftSide}>
+      <Box component="div" className={classes.cover}>
+      <Box component="img" className={classes.image} src={BubbleSvg} alt="bubble chat" />
+      <Typography className={classes.title}>Converse with anyone</Typography>
+      <Typography className={classes.title}>with any language</Typography> 
+      </Box>   
+    </Box>)}
+    <Box component="div" className={classes.rightSide}>
+      <Box component="div" className={classes.header}>
+        <Typography>Already have an account?</Typography>
+        <Button color="inherit" className={classes.registerButton} onClick={() => history.push("/login")}>Login</Button>
+      </Box>
+      <Box component="form" className={classes.form} onSubmit={handleRegister}>
+        <Box component="div" className={classes.titleFormContainer}>
+          <Typography className={classes.titleForm}>Create an account.</Typography>
+        </Box>
+        <Box component="div" className={classes.inputContainer}>
+        <FormControl margin="normal" required>
                 <TextField
+                  className={classes.input}
                   aria-label="username"
                   label="Username"
                   name="username"
                   type="text"
-                  required
                 />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl>
+        </FormControl>
+          <FormControl margin="normal" required>
                 <TextField
+                  className={classes.input}
+                  aria-label="email"
                   label="E-mail address"
-                  aria-label="e-mail address"
-                  type="email"
                   name="email"
-                  required
+                  type="text"
                 />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  aria-label="password"
-                  label="Password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="password"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  label="Confirm Password"
-                  aria-label="confirm password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="confirmPassword"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Button type="submit" variant="contained" size="large">
-              Create
-            </Button>
-          </Grid>
-        </form>
+        </FormControl>
+        <FormControl margin="normal" required>
+              <TextField
+                className={classes.input}
+                label="Password"
+                aria-label="password"
+                fullWidth
+                type="password"
+                name="password"
+              />
+            </FormControl>
+        </Box>
+        <Button color="primary" className={classes.buttonSubmit} type="submit" variant="contained" size="large">
+              Login
+        </Button>
       </Box>
-    </Grid>
+    </Box>
+  </Box>
   );
 };
 
@@ -121,4 +202,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
